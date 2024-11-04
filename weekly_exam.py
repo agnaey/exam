e5456aeb9aad25b77d676c00cd7e867c80cdbaa1
agnaey <agnaeyviswanath1234@gmail.com>
2024-11-04 13:33:38 +0530

    monday exam

---
+++ weekly_exam.py
@@ -0,0 +1,27 @@
+##longest in string 
+# l=['kiwi','apple','orange']
+# longest=l[0]
+# for i in l:
+    # if len(i)>len(longest):
+        # longest=i
+# print(longest)
+        
+
+##largest no  
+      
+# l=[2,11,3,4,6]
+# largest=l[0]
+
+# for i in l:
+#     if i > largest:
+#         largest=i
+# print(largest)
+
+## rev
+
+rev=input('enter the string : ')
+string=0
+
+while rev>string:
+    print(rev[::-1])
+    string+=1
